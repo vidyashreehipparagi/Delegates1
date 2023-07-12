02635d2fe23458eb73ba517ba08b65162f0d59ea
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-12 16:58:41 +0530

    Add project files.

---
+++ Delegates1/Calculation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    public delegate int MyDelegate(int x, int y);
+    public class Calculation
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+      
+    }
+    public delegate void Delegate1(int id, string name, double sal);
+    public class Test
+    {
+        public void AcceptDetails(int id, string name, double sal)
+        {
+            Console.WriteLine($"{id}-->{name}-->{sal}");
+        }
+    }
+}
---
+++ Delegates1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c= new Calculation();
+            MyDelegate mydel = new MyDelegate(c.Add);
+            Console.WriteLine(mydel.Invoke(30, 30));
+            Test t= new Test();
+            Delegate1 d = new Delegate1(t.AcceptDetails);
+            d.Invoke(1, "Vidyashree", 50000);
+        }
+    }
+}
